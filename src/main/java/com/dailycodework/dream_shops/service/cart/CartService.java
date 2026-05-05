3449a05e27d5bba4b2a14e68b245e999ab34519c
san.nt <san.nt@shopdunk.com>
2026-05-05 17:10:54 +0700

    completed cart,cartItem service impl

--- src/main/java/com/dailycodework/dream_shops/service/cart/CartService.java
+++ src/main/java/com/dailycodework/dream_shops/service/cart/CartService.java
@@ -2,6 +2,7 @@ package com.dailycodework.dream_shops.service.cart;
 
 import com.dailycodework.dream_shops.exceptions.ResourcesNotFoundException;
 import com.dailycodework.dream_shops.model.Cart;
+import com.dailycodework.dream_shops.repository.CartItemRepository;
 import com.dailycodework.dream_shops.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ import java.math.BigDecimal;
 public class CartService implements ICartService {
 
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
     @Override
     public Cart getCart(Long id) {
         Cart cart = cartRepository.findById(id).orElseThrow(() -> new ResourcesNotFoundException("Cart not found"));
@@ -23,11 +26,15 @@ public class CartService implements ICartService {
 
     @Override
     public void clearCart(Long id) {
-
+        Cart cart = getCart(id);
+        cartItemRepository.deleteAllByCartId(id);
+        cart.getItems().clear();
+        cartRepository.deleteById(id);
     }
 
     @Override
     public BigDecimal getTotalPrice(Long id) {
-        return null;
+        Cart cart = getCart(id);
+        return cart.getTotalAmount();
     }
 }
